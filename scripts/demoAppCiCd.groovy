60a15c832d261898699d3638746d0a218ca573b8
Ram Kannan <Ram_Kannan@symantec.com>
2019-02-26 16:00:33 +0530

    updating files

--- scripts/demoAppCiCd.groovy
+++ scripts/demoAppCiCd.groovy
@@ -7,8 +7,8 @@ def jobName = "DemoApp_Ci_Cd"
 def desc = "DemoApp Workshop CI CD Flow"
 def pipelinScript = "./scripts/DemoApp_Ci_Cd_Pipeline"
 
-def nexusURL = "http://100.122.214.160:8081/"
-def tomcatPATH = "http://100.122.214.160:8080/"
+def nexusURL = "http://100.122.214.160:8081"
+def tomcatPATH = "/usr/local/tomcat9"
 
 
 pipelineJob(jobName) {  
