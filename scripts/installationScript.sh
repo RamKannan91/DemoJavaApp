ad977811386f3b916e41ffe1044de91cbd4f2e51
ram_kannan <ramkannan1991@gmail.com>
2019-03-15 16:53:41 +0530

    Update installationScript.sh

--- scripts/installationScript.sh
+++ scripts/installationScript.sh
@@ -2,7 +2,7 @@
 
 #### Docker installtion
 yum update -y
-yum install git ansible dejavu-sans-fonts fontconfig xorg-x11-server-Xvfb -y
+yum install git ansible wget dejavu-sans-fonts fontconfig xorg-x11-server-Xvfb -y
 curl -fsSL https://get.docker.com/ | sh
 service docker start
 
