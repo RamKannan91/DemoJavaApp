e7c1f13a6eaf4bd5cf048cb2065bcc443b8ab492
ram_kannan <ramkannan1991@gmail.com>
2019-03-19 12:00:21 +0530

    Update installationScript.sh

--- scripts/installationScript.sh
+++ scripts/installationScript.sh
@@ -79,5 +79,10 @@ sed -i -e 's/8080/8502/g' /usr/local/tomcat/conf/server.xml
 /usr/local/tomcat/bin/startup.sh
 
 ### openshift
-yum -y install centos-release-openshift-origin310 epel-release git pyOpenSSL
+yum -y install centos-release-openshift-origin310 epel-release pyOpenSSL
+wget https://github.com/openshift/origin/releases/download/v3.11.0/openshift-origin-client-tools-v3.11.0-0cbc58b-linux-64bit.tar.gz
+tar -xvf openshift-origin-client-tools-v3.11.0-0cbc58b-linux-64bit.tar.gz
+mv openshift-origin-client-tools-v3.11.0-0cbc58b-linux-64bit oc-tool
+cd oc-tool
+export PATH=$HOME/oc-tool:$PATH
 
