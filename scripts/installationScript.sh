5a510a09753324d0ad2d38be91a60570ca47bdcb
ram_kannan <ramkannan1991@gmail.com>
2019-03-19 15:53:27 +0530

    Update installationScript.sh

--- scripts/installationScript.sh
+++ scripts/installationScript.sh
@@ -79,10 +79,19 @@ sed -i -e 's/8080/8502/g' /usr/local/tomcat/conf/server.xml
 /usr/local/tomcat/bin/startup.sh
 
 ### openshift
-yum -y install centos-release-openshift-origin310 epel-release pyOpenSSL
+yum -y install centos-release-openshift-origin310 origin-clients epel-release pyOpenSSL
 wget https://github.com/openshift/origin/releases/download/v3.11.0/openshift-origin-client-tools-v3.11.0-0cbc58b-linux-64bit.tar.gz
 tar -xvf openshift-origin-client-tools-v3.11.0-0cbc58b-linux-64bit.tar.gz
 mv openshift-origin-client-tools-v3.11.0-0cbc58b-linux-64bit oc-tool
 cd oc-tool
 export PATH=$HOME/oc-tool:$PATH
 
+cat << EOF >/etc/docker/daemon.json
+{
+ "insecure-registries": [
+ "172.30.0.0/16"
+ ]
+}
+EOF
+service docker restart
+oc cluster up
